354424ccad8bbd55529775feb900978bd5e4f788
pp35 <poornimapandey3590.com>
2024-09-30 15:21:00 +0530

    first commit

--- program1.js
+++ program1.js
@@ -4,8 +4,32 @@
  */
 var isValid = function(s) {
     
+    const brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+   
+    const stack = [];
+    
+   
+    for (let char of s) {
+        if (brackets[char]) {
+            
+            const bracket = stack.length > 0 ? stack.pop() : '#';  
+            if (brackets[char] !== bracket) {
+                return false;  
+            }
+        } else {
+            
+            stack.push(char);
+        }
+    }
+    
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
+//console.log(isValid("()")); 
 
--- program2.js
+++ program2.js
@@ -3,8 +3,41 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+   
+    const romanNo = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    
+    let total = 0
+    
+  
+    for (let i = 0; i < s.length; i++) {
+    
+        const currentValue = romanNo[s[i]]
+    
+        const nextValue = romanNo[s[i + 1]]
+        
+      
+        if (nextValue > currentValue) {
+            total -= currentValue
+        } else {
+           
+            total += currentValue
+        }
+    }
+    
+   
+    return total;
 };
 
+module.exports = { romanToInt }
 
-module.exports={romanToInt}
+
+// console.log(romanToInt("III"))     
